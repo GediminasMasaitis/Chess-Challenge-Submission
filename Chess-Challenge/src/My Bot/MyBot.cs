bc3d3f85dccc38ce0ae3cc028094eeb44e25afaf
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-24 23:22:59 +0800

    1.35.3 - Reduce token count

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -9,6 +9,7 @@ public class MyBot : IChessBot
 
     long nodes = 0; // #DEBUG
     long[] quietHistory = new long[4096];
+    Move[] killers = new Move[256];
 
     const int TTSize = 1048576;
     // Key, move, depth, score, flag
@@ -23,19 +24,17 @@ public class MyBot : IChessBot
     ulong[] pstRanks = { 0, 32125521998837248, 16501772952854001903, 18086463821592985848, 796584101102809849, 864419553310604285, 17729565680195270900 };
     ulong[] pstFiles = { 0, 18016933991885308669, 17654403052536658922, 18304035169612464635, 17725610693920817404, 794607157805906418, 17943749385544467700 };
 
-    int[] mobilities = { 6, 5, 3, 0 };
-
     private int Evaluate(Board board)
     {
         int score = 0;
-        for (var color = 0; color < 2; color++)
+        for (var color = 2; --color >= 0; score = -score)
         {
             var isWhite = color == 0;
 
-            for (var piece = PieceType.Pawn; piece <= PieceType.King; piece++)
+            //       None (skipped)               King
+            for (var pieceIndex = 0; ++pieceIndex <= 6;)
             {
-                var pieceIndex = (int)piece;
-                var bitboard = board.GetPieceBitboard(piece, isWhite);
+                var bitboard = board.GetPieceBitboard((PieceType)pieceIndex, isWhite);
 
                 if (pieceIndex == 3 && BitboardHelper.GetNumberOfSetBits(bitboard) == 2) // Bishop pair
                     score += 48;
@@ -46,31 +45,23 @@ public class MyBot : IChessBot
 
                     // Mobility
                     if (pieceIndex > 2)
-                        score += mobilities[pieceIndex - 3] * BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetPieceAttacks(piece, new Square(sq), board, isWhite) & ~(isWhite ? board.WhitePiecesBitboard : board.BlackPiecesBitboard));
+                        score += (-pieceIndex - 1) * (pieceIndex - 6) * BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetPieceAttacks((PieceType)pieceIndex, new Square(sq), board, isWhite) & ~(isWhite ? board.WhitePiecesBitboard : board.BlackPiecesBitboard)) / 2;
 
                     // Flip square if black
                     sq ^= 56 * color;
 
                     // Material
-                    score += material[pieceIndex];
-
-                    // Rank PST
-                    score += (sbyte)(pstRanks[pieceIndex] >> (sq / 8 * 8) & 0xFF) * 2;
-
-                    // File PST
-                    score += (sbyte)(pstFiles[pieceIndex] >> (sq % 8 * 8) & 0xFF) * 2;
-
-                    
+                    score += material[pieceIndex]
+                          +  (sbyte)(pstRanks[pieceIndex] >> (sq / 8 * 8) & 0xFF) * 2  // Rank PST
+                          +  (sbyte)(pstFiles[pieceIndex] >> (sq % 8 * 8) & 0xFF) * 2; // File PST
                 }
             }
-
-            score = -score;
         }
 
-        return board.IsWhiteToMove ? score : -score;
+        return board.IsWhiteToMove ? -score : score;
     }
 
-    private int Search(Board board, Timer timer, int allocatedTime, int ply, int depth, int alpha, int beta, Move[] killers, bool nullAllowed, out Move bestMove)
+    private int Search(Board board, Timer timer, int allocatedTime, int ply, int depth, int alpha, int beta, bool nullAllowed, out Move bestMove)
     {
         ulong key = board.ZobristKey;
         bestMove = Move.NullMove;
@@ -110,7 +101,7 @@ public class MyBot : IChessBot
             if (nullAllowed && staticScore >= beta && depth > 2)
             {
                 board.ForceSkipTurn();
-                var score = -Search(board, timer, allocatedTime, ply + 1, depth - 4, -beta, -beta + 1, killers, false, out _);
+                var score = -Search(board, timer, allocatedTime, ply + 1, depth - 4, -beta, -beta + 1, false, out _);
                 board.UndoSkipTurn();
                 if (score >= beta)
                     return beta;
@@ -162,7 +153,7 @@ public class MyBot : IChessBot
                           : 1;
 
             doSearch:
-            var score = -Search(board, timer, allocatedTime, ply + 1, depth - reduction, -childAlpha, -alpha, killers, true, out _);
+            var score = -Search(board, timer, allocatedTime, ply + 1, depth - reduction, -childAlpha, -alpha, true, out _);
 
             // If score raises alpha, we see if we should do a re-search
             if (score > alpha)
@@ -238,19 +229,19 @@ public class MyBot : IChessBot
         // Decay quiet history instead of clearing it
         for (var i = 0; i < 4096; quietHistory[i++] /= 8) ;
 
-        var killers = new Move[256];
+        Array.Clear(killers);
         var bestMove = Move.NullMove;
         var score = 0;
         nodes = 0; // #DEBUG
         // Iterative deepening
-        for (var depth = 1; depth < 128; depth++)
+        for (var depth = 0; ++depth < 128;)
         {
             // Aspiration windows
             var window = 40;
             research:
 
             // Search with the current window
-            var newScore = Search(board, timer, allocatedTime, 0, depth, score - window, score + window, killers, false, out var move);
+            var newScore = Search(board, timer, allocatedTime, 0, depth, score - window, score + window, false, out var move);
 
             // Hard time limit
             // If we are out of time, we cannot trust the move that was found
